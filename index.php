075c9e9ebe27ca31a1da633d456ccbebbf2375e7
angelvanesa <61381670+angelvanesa@users.noreply.github.com>
2020-04-15 19:31:18 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<?php
+	$fkw = "Trik Rahasia Judi Online";
+	$ufkw = "";
+	$title = "AgenSBO - Mengupas trik permainan judi SBOBet online secara tuntas";
+	$desc = "AgenSBO akan mengupas berbagai trik rahasia untuk berbagai jenis permainan judi SBOBet online dengan fokus kemenangan yang stabil";
+	$key = "Agen SBO, Agen SBOBet, Bola Online, Casino Online, Slot Online";
+?>
+<html ⚡ lang="id-ID">
+<head>
+	<?php include 'lib/head.html';?>
+	<script custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js" async=""></script>
+	<script custom-element="amp-image-lightbox" src="https://cdn.ampproject.org/v0/amp-image-lightbox-0.1.js" async=""></script>
+	<style amp-custom=""><?php include 'css/main.css';?><?php include 'css/custom.css';?></style>
+</head>
+<body>
+	<?php include 'lib/sidenav.html';?>
+	<?php include 'content/home.html';?>
+	<?php include 'lib/footer.html';?>
+</body>
+</html>
